b8a0eb4e62995ea473d6f9cc6fb7d64b827cc89d
PRESALEGEM <markmon050820@gmail.com>
2025-04-06 03:39:48 +0000

    ton manifest

--- app/providers.tsx
+++ app/providers.tsx
@@ -2,7 +2,7 @@
 
 import { TonConnectUIProvider } from '@tonconnect/ui-react';
 
-const manifestUrl = 'https://markmon08.github.io/gemspider/tonconnect-manifest.json';
+const manifestUrl = 'https://gemspiderofficial.github.io/gemspider/tonconnect-manifest.json';
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
